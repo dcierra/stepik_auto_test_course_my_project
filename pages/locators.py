49745b22b59b93c3e8fea95f612dfffb05c1910f
dcierra <1cebergg@mail.ru>
2022-02-06 16:40:31 +0700

    add login page tests

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.XPATH, "//a[@id='login_link']")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.XPATH, "//form[@id='login_form']")
+    REGISTER_FORM = (By.XPATH, "//form[@id='register_form']")
--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,14 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
+    login_form = LoginPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_form.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
