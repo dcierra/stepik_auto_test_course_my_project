d90a42b72078a5be72c4ea1adf504ca6fb9e7064
dcierra <1cebergg@mail.ru>
2022-02-06 19:53:19 +0700

    add tests with negative checks and transition to login form

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.XPATH, "//a[@id='login_link']")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.XPATH, "//form[@id='login_form']")
     REGISTER_FORM = (By.XPATH, "//form[@id='register_form']")
@@ -16,3 +12,9 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.XPATH, "//div[@class='col-sm-6 product_main']/h1")
     MESSAGE_PRODUCT_NAME = (By.XPATH, "//div[@class='alert alert-safe alert-noicon alert-success  fade in']//strong")
     BASKET_PRICE = (By.XPATH, "//div[@class='alert alert-safe alert-noicon alert-info  fade in']//strong")
+    SUCCESS_MESSAGE = (By.XPATH, "//div[@class='alert alert-safe alert-noicon alert-success  fade in']")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.XPATH, "//a[@id='login_link']")
+    LOGIN_LINK_INVALID = (By.XPATH, "//a[@id='login_link_inc']")
--- pages/product_page.py
+++ pages/product_page.py
@@ -12,6 +12,13 @@ class ProductPage(BasePage):
     def should_be_product_price(self):
         assert self.browser.find_elements(*ProductPageLocators.PRODUCT_PRICE), "Product Price is not presented"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_success_msg_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not disappeared"
+
     def should_be_product_name_in_msg(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         product_name_in_msg = self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_NAME).text
@@ -28,7 +35,3 @@ class ProductPage(BasePage):
         self.should_be_product_price()
         btn = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         btn.click()
-        self.solve_quiz_and_get_code()
-        self.should_be_product_name_in_msg()
-        self.compare_product_price_and_basket_price()
-
